fa841be8469401d863cd1f00427d833a8f55401d
Carter Pollan <carter.pollan@gmail.com>
2022-04-29 17:52:22 -0600

    Changed output to a text file

--- trace.py
+++ trace.py
@@ -5,6 +5,13 @@ target = 'google.com'
 if len(sys.argv) == 2:
     target = sys.argv[1]
 
-ans, unans = scapy.sr(scapy.IP(dst=target, ttl=(1,30),id=scapy.RandShort())/scapy.TCP(flags=0x2), timeout=3)
+ans, unans = scapy.sr(scapy.IP(dst=target, ttl=(1,21),id=scapy.RandShort())/scapy.TCP(flags=0x2), timeout=.5)
 for snd,rcv in ans:
-    print(snd.ttl, rcv.src, isinstance(rcv.payload, scapy.TCP))
+    with open('/home/carter/VS Studio/Trace IP Address/output.txt', 'a') as f:
+        toAdd = '\n'
+        if snd.ttl < 10:
+            toAdd += '0' + str(snd.ttl)
+        else:
+            toAdd += str(snd.ttl)
+        toAdd += ' ' + str(rcv.src)
+        f.write(toAdd)
