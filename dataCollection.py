bed0d71926d74151a6279f1e01ef6b8b523d0f48
Carter Pollan <carter.pollan@gmail.com>
2022-05-03 13:43:53 -0600

    Created the data structure inside of Python

--- dataCollection.py
+++ dataCollection.py
@@ -1,5 +1,15 @@
 from trace import traceRoute
+data = {
+    "IPADR1": [ 
+        "google.com",
+        ["2022-5-3 13:54", [], -1],
+        ["2022-5-3 13:54", [], -1]],
+    "IPADR2": [ 
+        "yahoo.com",
+        ["2022-5-3 13:54", [], -1],
+        ["2022-5-3 13:54", [], -1]
+    ]
+}
 
 
-
-traceRoute('google.com')
+data["IPADR1"][1][1] = traceRoute(data["IPADR1"][0])
