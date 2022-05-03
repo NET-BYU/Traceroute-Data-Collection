177f1880cc5009bb866eac2603f948f4651079e0
Carter Pollan <carter.pollan@gmail.com>
2022-05-03 13:41:46 -0600

    Changes the output of the traceRoute function from text to a list

--- trace.py
+++ trace.py
@@ -3,13 +3,9 @@ import scapy.all as scapy
 
 
 def traceRoute(target):
+
     ans, unans = scapy.sr(scapy.IP(dst=target, ttl=(1,21),id=scapy.RandShort())/scapy.TCP(flags=0x2), timeout=.5)
+    output = []
     for snd,rcv in ans:
-        with open('/home/carter/VS Studio/Trace IP Address/output.txt', 'a') as f:
-            toAdd = '\n'
-            if snd.ttl < 10:
-                toAdd += '0' + str(snd.ttl)
-            else:
-                toAdd += str(snd.ttl)
-            toAdd += ' ' + str(rcv.src)
-            f.write(toAdd)
+        output.append(rcv.src)
+    return output
