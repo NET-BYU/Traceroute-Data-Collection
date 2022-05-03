aaeeb50b228f58e0902e757db861d0600540bad0
Carter Pollan <carter.pollan@gmail.com>
2022-05-03 12:58:31 -0600

    converted traceroute to a function that is called in dataCollection.py

---
+++ dataCollection.py
@@ -0,0 +1,17 @@
+from trace import traceRoute
+
+data = {
+    "IPADR1": [ 
+        "google.com",
+        ["TIME 12:32", [], -1],
+        ["TIME 12:24", [], -1]],
+    "IPADR2": [ 
+        "yahoo.com",
+        ["TIME 12:24", [], -1],
+        ["TIME 12:24", [], -1]
+    ]
+}
+
+
+
+#traceRoute('google.com')
--- trace.py
+++ trace.py
@@ -1,17 +1,22 @@
+import json
 import sys
 import scapy.all as scapy
 
-target = 'google.com'
-if len(sys.argv) == 2:
-    target = sys.argv[1]
 
-ans, unans = scapy.sr(scapy.IP(dst=target, ttl=(1,21),id=scapy.RandShort())/scapy.TCP(flags=0x2), timeout=.5)
-for snd,rcv in ans:
-    with open('/home/carter/VS Studio/Trace IP Address/output.txt', 'a') as f:
-        toAdd = '\n'
-        if snd.ttl < 10:
-            toAdd += '0' + str(snd.ttl)
-        else:
-            toAdd += str(snd.ttl)
-        toAdd += ' ' + str(rcv.src)
-        f.write(toAdd)
+
+
+
+
+
+def traceRoute(target):
+    ans, unans = scapy.sr(scapy.IP(dst=target, ttl=(1,21),id=scapy.RandShort())/scapy.TCP(flags=0x2), timeout=.5)
+    for snd,rcv in ans:
+        with open('/home/carter/VS Studio/Trace IP Address/output.txt', 'a') as f:
+            toAdd = '\n'
+            if snd.ttl < 10:
+                toAdd += '0' + str(snd.ttl)
+            else:
+                toAdd += str(snd.ttl)
+            toAdd += ' ' + str(rcv.src)
+            f.write(toAdd)
+
