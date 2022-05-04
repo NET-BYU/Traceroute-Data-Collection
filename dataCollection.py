c71f8ddf83065a2464517d79eef1279dcaaf5fab
Carter Pollan <carter.pollan@gmail.com>
2022-05-04 17:04:31 -0600

    Changed some formating things

--- dataCollection.py
+++ dataCollection.py
@@ -1,8 +1,13 @@
+import datetime
+from threading import currentThread
+
 from trace import traceRoute
+
+
 data = {
     "IPADR1": [ 
         "google.com",
-        ["2022-5-3 13:54", [], -1],
+        ["2022-5-3 13:54", [], []],
         ["2022-5-3 13:54", [], -1]],
     "IPADR2": [ 
         "yahoo.com",
@@ -13,3 +18,6 @@ data = {
 
 
 data["IPADR1"][1][1] = traceRoute(data["IPADR1"][0])
+data["IPADR1"][1][0] = datetime.datetime.now()
+for x in data["IPADR1"][1][1]:
+    print(x)
