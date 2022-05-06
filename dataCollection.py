d9e3631d3df128cef42648abfd31bdeb303e95c1
Carter Pollan <carter.pollan@gmail.com>
2022-05-06 12:02:32 -0600

    Moved the getData funtion to trace.py and formatted everything with the Black Code Style

--- dataCollection.py
+++ dataCollection.py
@@ -1,23 +1,10 @@
-import datetime
 import subprocess
-from trace import traceRoute, GetTTL
+from trace import getData
 
 
-IPADR = [
-    "8.8.8.8",
-    "yahoo.com",
-    "google.com",
-    "firefox.com",
-    "miniclip.com"]
+IPADR = ["8.8.8.8", "yahoo.com", "google.com", "firefox.com", ""]
 
-def getData(target):
-    output = str(datetime.datetime.now().strftime("%x")) + " " + str(datetime.datetime.now().strftime("%X"))+ "\t" + traceRoute(target) + "\t" + str(GetTTL(target))
-    return output
 
 for target in IPADR:
     with open("./Outputs/" + target + ".txt", "a") as this:
         this.write(getData(target) + "\n")
-    
-
-
-
--- trace.py
+++ trace.py
@@ -1,59 +1,61 @@
 import subprocess
+import time
 import os
 
 
 def traceRoute(target):
-  p = subprocess.run(["traceroute", target, "-n", "-q", "1"], stdout=subprocess.PIPE, text=True)
-  
-  return ParseTraceRouteOutput(p)
-
-
+    p = subprocess.run(
+        ["traceroute", target, "-n", "-q", "1"], stdout=subprocess.PIPE, text=True
+    )
+    return ParseTraceRouteOutput(p)
 
 
 def ParseTraceRouteOutput(input):
 
-  output = ""
-  traceroute = []
-  latency = []
+    output = ""
+    traceroute = []
+    latency = []
 
-  splitter = input.stdout.split("\n")
-  del splitter[0]
+    splitter = input.stdout.split("\n")
+    del splitter[0]
 
+    for x in splitter:
+        x = x[4 : len(x)]
 
-  for x in splitter:
-      x = x[4:len(x)]
+        if x == "":
+            continue
 
-      if x == '':
-          continue
-        
-      if x == "*":
-          traceroute.append("*")
-          latency.append("*")
-      else:
-          smallSplitter = x.split(" ")
-          traceroute.append(smallSplitter[0])
-          latency.append(smallSplitter[2])
+        if x == "*":
+            traceroute.append("*")
+            latency.append("*")
+        else:
+            smallSplitter = x.split(" ")
+            traceroute.append(smallSplitter[0])
+            latency.append(smallSplitter[2])
 
+    for x in traceroute:
+        output = output + x + " "
+    output = output[0 : len(output) - 2]
+    output = output + "\t"
 
-  for x in traceroute:
-    output = output+x+", "
-  output = output[0:len(output)-2]
-  output = output + "\t"
-  
-  for x in latency:
-    output = output + x + ", "
-  output = output[0:len(output)-2]
+    for x in latency:
+        output = output + x + " "
+    output = output[0 : len(output) - 2]
 
-  return output
-
+    return output
 
 
 def GetTTL(ip):
-  result = os.popen("ping -c 1 "+ip).read()
-  n = result.find("ttl=")
-  if n >= 0:
-    ttl = result[n+4:]
-    n = ttl.find(" ")
-    if n > 0:
-      return int(ttl[:n])
-  return -1
+    result = os.popen("ping -c 1 " + ip).read()
+    n = result.find("ttl=")
+    if n >= 0:
+        ttl = result[n + 4 :]
+        n = ttl.find(" ")
+        if n > 0:
+            return int(ttl[:n])
+    return -1
+
+
+def getData(target):
+    output = str(time.time()) + "\t" + str(GetTTL(target)) + "\t" + traceRoute(target)
+    return output
