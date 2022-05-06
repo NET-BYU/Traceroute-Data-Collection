e00eb6b8ec82a74dfac652c4d0103cfe85032932
Carter Pollan <carter.pollan@gmail.com>
2022-05-06 15:37:06 -0600

    init commit

--- dataCollection.py
+++ dataCollection.py
@@ -1,10 +1,13 @@
-import subprocess
+import time
 from trace import getData
 
 
-IPADR = ["8.8.8.8", "yahoo.com", "google.com", "firefox.com", ""]
+IPADR = ["8.8.8.8", "yahoo.com", "google.com", "firefox.com"]
 
 
-for target in IPADR:
-    with open("./Outputs/" + target + ".txt", "a") as this:
-        this.write(getData(target) + "\n")
+while True:
+    for target in IPADR:
+        with open("./Outputs/" + target + ".txt", "a") as this:
+            this.write(getData(target) + "\n")
+    print("finished 1 traceroute")
+    time.sleep(60 * 10)
