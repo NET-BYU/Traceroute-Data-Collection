50a4373a67c1930c6f47e6107b2f5a79620b52f3
Carter Pollan <carter.pollan@gmail.com>
2022-05-16 15:44:32 -0600

    Seperated the graphTraceroute and graphPintLatency functions into their own file and renamed them

--- dataAnalysis/dataAnalysis.py
+++ dataAnalysis/dataAnalysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import graph
 import matplotlib.pyplot as plt
 
 data = pd.read_csv(
@@ -15,46 +16,5 @@ data["Traceroute"] = data["Traceroute"].str.split(" ")
 data["Delay"] = data["Delay"].str.split(" ")
 
 
-def graphPingLatency(list_of_latencys):
-
-    array_of_latency = np.array(list_of_latencys)
-    array_of_latency = np.sort(array_of_latency)
-
-    fig, ax = plt.subplots()
-    plt.hist(array_of_latency, bins=100)
-    ax.set_title("Ping Latency")
-    ax.set_ylabel("Instances in Range")
-    ax.set_xlabel("Latency")
-    plt.show()
-
-
-def graphTraceroute(list_of_traceroutes):
-    comprehensive_list_of_traceroutes = []
-
-    for traceroute in list_of_traceroutes:
-        for index in range(len(traceroute)):
-            if (len(comprehensive_list_of_traceroutes) - 1) <= index:
-                comprehensive_list_of_traceroutes.append([traceroute[index]])
-            if traceroute[index] in comprehensive_list_of_traceroutes[index]:
-                pass
-            else:
-                comprehensive_list_of_traceroutes[index].append(traceroute[index])
-
-    print(comprehensive_list_of_traceroutes)
-    x = []
-    y = []
-    for index in range(len(comprehensive_list_of_traceroutes)):
-        x.append(index + 1)
-        y.append(len(comprehensive_list_of_traceroutes[index]))
-
-    fig, ax = plt.subplots()
-
-    ax.bar(x, y, width=1, edgecolor="white", linewidth=1)
-    ax.set_title("Unique IP Addresses Across Multiple Traceroutes")
-    ax.set_xlabel("Distance From Source")
-    ax.set_ylabel("Number of Unique IP Addresses")
-    plt.show()
-
-
-graphTraceroute(data["Traceroute"])
-graphPingLatency(data["Latency"])
+graph.Traceroute(data["Traceroute"])
+graph.PingLatency(data["Latency"])
---
+++ dataAnalysis/graph.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+def PingLatency(list_of_latencys):
+
+    array_of_latency = np.array(list_of_latencys)
+    array_of_latency = np.sort(array_of_latency)
+
+    fig, ax = plt.subplots()
+    plt.hist(array_of_latency, bins=100)
+    ax.set_title("Ping Latency")
+    ax.set_ylabel("Instances in Range")
+    ax.set_xlabel("Latency")
+    plt.show()
+
+
+def Traceroute(list_of_traceroutes):
+    comprehensive_list_of_traceroutes = []
+
+    for traceroute in list_of_traceroutes:
+        for index in range(len(traceroute)):
+            if (len(comprehensive_list_of_traceroutes) - 1) <= index:
+                comprehensive_list_of_traceroutes.append([traceroute[index]])
+            if traceroute[index] in comprehensive_list_of_traceroutes[index]:
+                pass
+            else:
+                comprehensive_list_of_traceroutes[index].append(traceroute[index])
+
+    print(comprehensive_list_of_traceroutes)
+    x = []
+    y = []
+    for index in range(len(comprehensive_list_of_traceroutes)):
+        x.append(index + 1)
+        y.append(len(comprehensive_list_of_traceroutes[index]))
+
+    fig, ax = plt.subplots()
+
+    ax.bar(x, y, width=1, edgecolor="white", linewidth=1)
+    ax.set_title("Unique IP Addresses Across Multiple Traceroutes")
+    ax.set_xlabel("Distance From Source")
+    ax.set_ylabel("Number of Unique IP Addresses")
+    plt.show()
