1156ca3fe61f688cc9b902c5b7584d2578b44b22
Carter Pollan <carter.pollan@gmail.com>
2022-05-16 15:57:38 -0600

    made dataAnalysis automaticaly loop through the output file for all of the IP Addresses

--- dataAnalysis/dataAnalysis.py
+++ dataAnalysis/dataAnalysis.py
@@ -6,19 +6,19 @@ import json
 
 with open("Input.json", "r") as input_file:
     IPADR = json.load(input_file)
+for target in IPADR:
 
-data = pd.read_csv(
-    f"Outputs/{IPADR[0]}.txt",
-    sep="\t",
-    parse_dates=True,
-    infer_datetime_format=True,
-    index_col=0,
-    names=["Time", "TTL", "Traceroute", "Delay", "Latency"],
-)
-data.index = pd.to_datetime((data.index.values * 1e9).astype(int))
-data["Traceroute"] = data["Traceroute"].str.split(" ")
-data["Delay"] = data["Delay"].str.split(" ")
+    data = pd.read_csv(
+        f"Outputs/{target}.txt",
+        sep="\t",
+        parse_dates=True,
+        infer_datetime_format=True,
+        index_col=0,
+        names=["Time", "TTL", "Traceroute", "Delay", "Latency"],
+    )
+    data.index = pd.to_datetime((data.index.values * 1e9).astype(int))
+    data["Traceroute"] = data["Traceroute"].str.split(" ")
+    data["Delay"] = data["Delay"].str.split(" ")
 
-
-graph.Traceroute(data["Traceroute"])
-graph.PingLatency(data["Latency"])
+    graph.Traceroute(data["Traceroute"])
+    graph.PingLatency(data["Latency"])
