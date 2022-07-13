eb1e2f4212b1038782e9e304fa8c7a4db930447c
Carter Pollan <carter.pollan@gmail.com>
2022-07-13 14:49:03 -0600

    Added in line processing for the input

--- dataAnalysis/machineLearning.py
+++ dataAnalysis/machineLearning.py
@@ -30,7 +30,6 @@ def parse_data(target):
         infer_datetime_format=True,
         names=["Time", "TTL", "Traceroute", "Delay", "Latency"],
     )
-    # print(data.head())
 
     del data["Latency"]
     del data["Time"]
@@ -38,22 +37,19 @@ def parse_data(target):
     data["Delay"] = data["Delay"].str.split(" ")
 
     for index in data.index:
-        while True:
-            try:
-                data.loc[index, "Traceroute"].remove("*")
-            except:
-                break
         if type(data.loc[index, "Traceroute"]) == type(random_list):
             while True:
                 try:
                     data.loc[index, "Traceroute"].remove("*")
                 except:
                     break
+
             if len(data.loc[index, "Traceroute"]) > 5:
                 reduced_traceroute = data.loc[index, "Traceroute"][-5:]
             else:
                 reduced_traceroute = data.loc[index, "Traceroute"]
             del data.loc[index, "Traceroute"][0:]
+
             for ip in reduced_traceroute:
                 broken_ip = ip.split(".")
                 for individual_ip in broken_ip:
@@ -64,14 +60,9 @@ def parse_data(target):
             while len(data.loc[index, "Traceroute"]) < 20:
                 data.loc[index, "Traceroute"].append(-1.0)
         else:
-            # print(f"Error at line {index}")
+            print(f"Error at line {index} at {target}.txt")
             data.drop(index, axis=0)
-            pass
-        # while True:
-        #     try:
-        #         data.loc[index, "Delay"].remove("*")
-        #     except:
-        #         break
+
         if (
             type(data.loc[index, "Delay"]) == type(random_list)
             and len(data.loc[index, "Delay"]) >= 5
@@ -84,17 +75,15 @@ def parse_data(target):
                     data.loc[index, "Traceroute"].append(float(delay))
                 else:
                     data.loc[index, "Traceroute"].append(0.0)
+
         if type(data.loc[index, "Traceroute"]) == type(random_list):
             if data.loc[index, "TTL"] != "*":
                 data.loc[index, "Traceroute"].append(float(data.loc[index, "TTL"]))
             else:
                 data.loc[index, "Traceroute"].append(0.0)
-        # return data.loc[index, "Traceroute"]
-        try:
-            if len(data.loc[index, "Traceroute"]) != 26:
-                data.drop(index, axis=0)
-        except:
-            data.drop(index, axis=0)
+
+        while len(data.loc[index, "Traceroute"]) < 26:
+            data.loc[index, "Traceroute"].append(0.0)
 
     data = data.drop(["TTL", "Delay"], axis=1)
     return data
@@ -122,7 +111,37 @@ def MLIP(array, length):
     output = shuffle(output)
     output.reset_index(inplace=True, drop=True)
     return output
-    # print(output)
+
+
+def inline(array, length, percent):
+    for i in range(len(array)):
+        if i == 0:
+            value = [True for x in range(len(array[i]))]
+            array[i]["Correct"] = value
+            good_data = array[i]
+        else:
+            value = [False for x in range(len(array[i]))]
+            array[i]["Correct"] = value
+            if i == 1:
+                bad_data = array[i]
+            else:
+                bad_data = pd.concat([bad_data, array[i]], ignore_index=True)
+
+    bad_data = shuffle(bad_data)
+    bad_data.reset_index(inplace=True, drop=True)
+
+    train = pd.concat(
+        [bad_data[: int(length * percent)], good_data[:length]], ignore_index=True
+    )
+    test = pd.concat(
+        [bad_data[int(length * percent) :], good_data[length:]], ignore_index=True
+    )
+    X_train = train["Traceroute"]
+    y_train = train["Correct"]
+    X_test = test["Traceroute"]
+    y_test = test["Correct"]
+
+    return X_train, X_test, y_train, y_test
 
 
 array = [
@@ -130,7 +149,11 @@ array = [
     parse_data(IPADR[1]),
     parse_data(IPADR[2]),
     parse_data(IPADR[3]),
+    parse_data(IPADR[4]),
+    parse_data(IPADR[5]),
+    parse_data(IPADR[6]),
 ]
+
 x = 0
 for i in array[3]["Traceroute"]:
     x += 1
@@ -140,27 +163,23 @@ for i in array[3]["Traceroute"]:
     except:
         print("TypeError: object of type 'float' has no len()", i, "line:", x)
 
-array = MLIP(array, 500)
 
+X_train, X_test, y_train, y_test = inline(array, 30, 0.5)
+X_train = X_train.to_list()
+y_train = y_train.to_list()
+X_test = X_test.to_list()
+y_test = y_test.to_list()
 
-X = array["Traceroute"].to_list()
-y = array["Correct"].to_list()
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.3,
-    random_state=42,
-)
+print(f"Training: {len(X_train)}")
+print(f"Test: {len(X_test)}")
+
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-
-# print(X_test[:10])
 
 rfc = RandomForestClassifier(n_estimators=200)
 rfc.fit(X_train, y_train)
 pred_rfc = rfc.predict(X_test)
 
-pred_rfc[:20]
 
 print(classification_report(y_test, pred_rfc))
